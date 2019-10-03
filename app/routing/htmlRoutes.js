1d0e07f5ec6c67c25d16cb6ca4e764c12656888b
Paola Stone <paola.stonemartinez@dell.com>
2019-10-03 12:27:18 -0400

    all conenctions working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,14 @@
 var path = require('path');
 
 module.exports = function(app){
-  //rout to home.html
-  app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  //route to survey
+  app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // route to home page
-  app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  //route to everything else
+  app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+  
 };
